09b2e49ee661ae04aca1a9f317a63d4479dd8f93
Yash Hegde <yash4success@gmail.com>
2023-03-24 12:05:06 +0530

    mouse events

--- app.js
+++ app.js
@@ -4,13 +4,27 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The final empire',
             author: 'Brandon sanderson',
-            age: '45'
+            age: '45',
+            x: 0,
+            y:0
+
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
-       }
+        },
+        handleEvent(e,data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+            
+        }
     }
 })
 app.mount('#app')

a03ef8affed9dc82b06ced055a46c038a779b48e
Yash Hegde <yash4success@gmail.com>
2023-03-24 15:47:50 +0530

    attributebinding,dynamicclasses,computedproperty

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const app = Vue.createApp({
     data() {
         return {
+
+            url: 'https://github.com/Yh010',
             showBooks: true,
-            title: 'The final empire',
+            books: [
+                { title: 'Richest Man in Babylon', author: 'George S Clason', img: 'assets/download1.jpg',isFav:true},
+                { title: 'Relentless', author: 'Tim Grover', img: 'assets/download2.jpg',isFav:false},
+                { title: 'Elon Musk', author: 'Ashlee Vance', img: 'assets/download3.jpg',isFav:true},               
+            ]
+            
+            /*title: 'The final empire',
             author: 'Brandon sanderson',
             age: '45',
             x: 0,
-            y:0
+            y:0 */
 
         }
     },
@@ -24,7 +32,18 @@ const app = Vue.createApp({
             this.x = e.offsetX
             this.y = e.offsetY
             
+        }, 
+        toggleIsFav(book) {
+            book.isFav =! book.isFav 
+        },
+        computed: {
+            filteredBooks() {
+                return this.books.filter((book)=> book.isFav)
+            }
         }
+
+
+
     }
 })
 app.mount('#app')
